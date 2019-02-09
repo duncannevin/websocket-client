5989434420a134a2c5ce2c01d6823e00da82eb8d
Duncan Nevin <duncan.nevin@gmail.com>
2019-02-09 16:20:10 -0500

    WIP: starts fe auth

--- ui/src/components/Navbar.vue
+++ ui/src/components/Navbar.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
-    <b-navbar variant="faded" type="dark">
-      <b-navbar-brand href="#">Awesome Websocket Client</b-navbar-brand>
-    </b-navbar>
-  </div>
+  <b-navbar variant="faded" type="dark">
+    <b-navbar-brand href="#">Awesome Websocket Client</b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item>Sign In</b-nav-item>
+    </b-navbar-nav>
+</b-navbar>
 </template>
 
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  computed: {
+    authenticated () {
+      return this.$store.getters.getAuthenticated
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="sass">
+@import "../styles/custom-bootstrap"
+.nav-link
+  position: relative
+  color: $white!important
+  font-weight: $bold
+  &::after
+    content: ""
+    position: absolute
+    left: 0
+    right: 0
+    top: 80%
+    height: 2px
+    background-color: $white
 </style>
