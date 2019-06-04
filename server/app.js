6ed24696abe19bb2a5df9d06d04445d35925a394
Duncan Nevin <duncan.nevin@gmail.com>
2019-06-04 17:24:44 -0400

    adds restart always to application

--- server/app.js
+++ server/app.js
@@ -48,11 +48,6 @@ app.use(function (req, res, next) {
   next(createError(404))
 })
 
-// Server index.html page when request to the root is made
-app.use('/', function (req, res, next) {
-  res.sendfile('../public/index.html')
-})
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
